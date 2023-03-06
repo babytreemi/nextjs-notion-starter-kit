2a52bf911d73c5274f7bb876e26c75f8340a2eb5
babytreemi <109142402+babytreemi@users.noreply.github.com>
2023-03-06 22:10:57 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '4c8a2d5604074d91ad8b8b42c72f1948',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'shuy‘s RESEARCH CLUB!!!!',
+  domain: 'https://www.notion.so/shuyhere/4c8a2d5604074d91ad8b8b42c72f1948?v=2885a8cfebba4f38a8914b47e84f67b0',
+  author: 'YANG SHU',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'shuyhere',
+  github: 'babytreemi',
+  linkedin: 'yang shu',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
