25d5eff27c18e168395244393bf5844dee507d37
babytreemi <109142402+babytreemi@users.noreply.github.com>
2023-03-06 22:59:32 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'shuyhere',
+  twitter: '@E7doryH5Sd54olW',
   github: 'babytreemi',
-  linkedin: 'yang shu',
+  linkedin: '#',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
